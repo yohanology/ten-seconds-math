698d038f48b2a56a588ee75bb2beddb452e41aaa
Yohan <yohanology@gmail.com>
2015-07-11 14:01:32 +0800

    starting to add options in bottom

--- js/application.js
+++ js/application.js
@@ -1,15 +1,6 @@
 $('document').ready(function(){
-
-// 10 seconds left countdown
-// question = random operation = random number + random number
-// when answer = question --> 1 second added and new random question
-// but countdown continue
-// countdown = 0 --> answer input replaced by game over text
 	
-	//wins variables
 	var totalWin = 0;
-
-	//timer variables
 	var secondsLeft = 10;
 	var addSecond = function() {
 		secondsLeft++;
@@ -17,11 +8,7 @@ $('document').ready(function(){
 	var minusSecond = function() {
 		secondsLeft--;
 	};
-
-
 	
-
-	//random question variables
 	var randQuestion = function() {
 		var randNumber1 = Math.floor(Math.random()*10);
 		var randNumber2 = Math.floor(Math.random()*10);
@@ -30,19 +17,12 @@ $('document').ready(function(){
 
 	$('#question').text(randQuestion());
 
-
-
-
-	//check answer
 	var checkAnswer = function() {
-	 	console.log('checking answer');
 	 	
 	 	var correctAnswer = eval($('#question').text());
 	 	var inputAnswer = Number($('#answer').val());
 
-	 	if ((totalWin === 0) && (correctAnswer === inputAnswer)) {
-	 		
-	 		console.log('1st success');
+	 	if ((totalWin === 0) && (correctAnswer === inputAnswer)) {	 		
 	 		$('#answer').val('');
 	 		console.log('1st input erased')
 	 		$('#question').text(randQuestion());
@@ -50,12 +30,10 @@ $('document').ready(function(){
 	 		totalWin++;
 	 		console.log('1st win', totalWin);
 
-	 		//timer
 	 		var timer = setInterval(function(){
 	 			minusSecond();
 	 			$('#countdown').text(secondsLeft);
 	 			console.log(secondsLeft);
-
 	 			if (secondsLeft <= 0) {
 	 				clearInterval(timer);
 	 				$('#answer').replaceWith('<h2>Game over! You got ' + totalWin + ' correct!</h2>');
@@ -78,7 +56,7 @@ $('document').ready(function(){
 		 	}
 	 };
 
-	// check question + generate random question
+
 	$(document).on('keyup', '#answer', function(){
 		checkAnswer();
 	});
